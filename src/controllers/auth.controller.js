8d5c9d103a7247ed27322c57ebf266a8390295be
bablikumari2 <anandbabli96@gmail.com>
2022-02-18 00:42:44 +0530

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,65 +1,95 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model")
+const {body,validationResult} = require('express-validator');
 
 const newToken = (user) => {
     return jwt.sign({ user }, process.env.JWT_SECRET_KEY);
   };
-const register = async (req,res) =>{
-    try{
-         // we will try to find the user with the email provided
-    let user = await User.findOne({ email: req.body.email }).lean().exec();
 
-    // if the user is found then it is an error
-    if (user)
-      return res.status(400).send({ message: "Please try another email" });
+  function valid(){
+    return [body("name").isLength({min:3}).withMessage("etleast 3 length is required"),
+    body("email").isEmail().withMessage("please enter valid email"),
+    body("password").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]{8,}/,).withMessage('Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long')
+];
+}
+function valid_2(){
+    return [body("email").isLength({min:3}).withMessage("etleast 3 length is required"),
+    body("password").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]{8,}/,).withMessage('Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long')]
+}
 
-    // if user is not found then we will create the user with the email and the password provided
-    user = await User.create(req.body);
 
-    // user = new User()
-    // user.email = req.body.email
-    // user.password = req.body.password
-    // user.save();
+const register = async (req, res) => {
 
-    // then we will create the token for that user
-    const token = newToken(user);
+    const errors = validationResult(req);
+    let finalErrors = null;
+    if (!errors.isEmpty()) {
+        finalErrors = errors.array().map(error => {
+            return {
+                param: error.param,
+                msg: error.msg,
+            }
+        })
+      return res.status(400).json({ errors:finalErrors });
+   }
 
-    // then return the user and the token
+    let user;
+    try {
+        // First we check if user with same email already exists
+        user = await User.findOne({email: req.body.email})
 
-    res.send({ user,token});
-    }catch(err){
-        res.status(500).send(err.message);
+        // if yes then we throw an error that email already exists
+        if (user) return res.status(400).send({message: "Please check your email and password"});
+        
+        // else we will create the user with the email and password 
+        // but before saving the password we need to hash it
+        user = await User.create(req.body);
+        
+        // we will create a token
+        const token = newToken(user)
+        
+        // we will send the token to the frontend
+        return res.status(200).send({user, token});
+
+    } catch (err) {
+        return res.status(500).send({message: "Sorry for inconvenience please try again later"});
     }
 }
 
-const login = async (req,res) =>{
-    try{ // we will try to find the user with the email provided
-        const user = await User.findOne({ email: req.body.email });
-    
-        // If user is not found then return error
-        if (!user)
-          return res
-            .status(400)
-            .send({ message: "Please try another email or password" });
-    
-        // if user is found then we will match the passwords
-        const match = user.checkPassword(req.body.password);
-    
-        if (!match)
-          return res
-            .status(400)
-            .send({ message: "Please try another email or password" });
-    
-        // then we will create the token for that user
-        const token = newToken(user);
-    
-        // then return the user and the token
-        res.send({ user, token });
-      } catch (err) {
-        res.status(500).send(err.message);
+const login = async (req, res) => {
+    const errors = validationResult(req);
+    let finalErrors = null;
+    if (!errors.isEmpty()) {
+        finalErrors = errors.array().map(error => {
+            return {
+                param: error.param,
+                msg: error.msg,
+            }
+        })
+      return res.status(400).json({ errors:finalErrors });
+   }
+    try {
 
+        let user = await User.findOne({email: req.body.email});
+
+        // if not exists we throw an error
+        if(! user) return res.status(400).send({message: "Please check your email and password"});
+    
+        // if it exists then we match the password
+        let match = user.checkPassword(req.body.password);
+    
+        // if not match then we throw an error
+        if(! match) return res.status(400).send({message: "Please check your email and password"});
+    
+        // we will create a token
+        const token = newToken(user)
+        
+        // we will send the token to the frontend
+        return res.status(200).send({user, token});
+
+    } catch (err) {
+        return res.status(500).send({message: "Sorry for inconvenience please try again later"});   
     }
 }
 
-module.exports = {register ,login}
+module.exports = {register, login,valid,valid_2}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
-
+const {check,validationResult,body} = require('express-validator');
 const connect = require("./configs/db")
 
+const postcontroller = require("./controllers/post.controller")
 const userController = require("./controllers/user.controller");
-const {register,login} = require("./controllers/auth.controller");
+
+const {register,valid,login,valid_2}=require("./controllers/auth.controller")
 const app = express()
 app.use(express.json());
 
-app.post("/register",register)
-app.post("/login",login)
-app.use("/users",userController);
+app.use("/register",valid(),register);
+app.use("/login", valid_2(),login);
+app.use("/posts", postcontroller)
 
 app.listen(2222,async() =>{
     try{
