067b0fbd904b9fd5298a7d965305bcb79eb09721
bablikumari2 <anandbabli96@gmail.com>
2022-02-17 23:54:15 +0530

    update

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const connect = require("./configs/db")
+
+const userController = require("./controllers/user.controller");
+const {register,login} = require("./controllers/auth.controller");
+const app = express()
+app.use(express.json());
+
+app.post("/register",register)
+app.post("/login",login)
+app.use("/users",userController);
+
+app.listen(2222,async() =>{
+    try{
+        await connect ();
+
+    }catch (err){
+        console.log(err.message)
+    }
+    console.log("listening on port 2222")
+});
